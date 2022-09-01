2fd210a8839cb8a78095029f175a6b657354d353
Oliver Shetler <oliver@olivershetler.com>
2022-09-01 12:45:04 -0400

    Created a main function in manage.py that implements a simple argument parser that gets the relevant directories and exectutes teh convert_all_files function. I'm getting a 'PermissionError: [Errno 13] Permission denied: './test_outputs'' and I don't know how to resolve it.

--- manage.py
+++ manage.py
@@ -1,4 +1,20 @@
-from src.intan_to_eeg_and_egf import intan_to_eeg_and_egf
+from src.converter import convert_all_files
 
 import argparse
 
+
+def main():
+    parser = argparse.ArgumentParser(description='Convert Intan .rhd files to .eeg  and .egf files. Optionally, also convert a .csv file with position data in conjunction with a header .json file to a .pos file.')
+
+
+    parser.add_argument('-o', '--output', help='Output directory to store .eeg, .egf and optional .pos files.', required=True)
+    parser.add_argument('-i', '--rhd', help='Input path containing an .rhd file.', required=True)
+    parser.add_argument('-p', '--csv', help='Input directory containing .csv file with position data.', required=False)
+    parser.add_argument('-j', '--json', help='Input directory containing .json file with header.', required=False)
+
+    args = parser.parse_args()
+
+    convert_all_files(args.rhd, args.output, args.csv, args.json)
+
+if __name__ == '__main__':
+    main()
