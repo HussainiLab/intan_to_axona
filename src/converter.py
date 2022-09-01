3110f8f436879ddc0ab5f8263aa3312fa93912b4
Oliver Shetler <oliver@olivershetler.com>
2022-09-01 12:13:41 -0400

    Finished writing the converter module. Wrote a test that just runs the converter function. Next, I need to create a simple cli and then the first draft is done!

--- src/converter.py
+++ src/converter.py
@@ -1,7 +1,11 @@
-from src.intan_to_eeg_and_egf import intan_to_eeg_and_egf
-from csv_to_pos import write_pos, pd_to_dict, text_to_dict
+import os
 
-def convert_all_files(target_directory, rhd_file_path, txt_or_csv_path=None):
+from src.intan_to_eeg_and_egf import intan_to_eeg_and_egf
+from src.csv_to_pos import write_pos, pd_to_dict, text_to_dict
+
+import pandas as pd
+
+def convert_all_files(target_directory, rhd_file_path, txt_or_csv_path=None, json_pos_path=None):
 
     assert os.path.exists(rhd_file_path), "The path to the .rhd file does not exist."
     assert os.path.exists(target_directory), "The path to the output directory does not exist."
@@ -11,17 +15,19 @@ def convert_all_files(target_directory, rhd_file_path, txt_or_csv_path=None):
 
     intan_to_eeg_and_egf(rhd_file_path, session_name, target_directory)
 
-    if txt_or_csv_path is not None:
-        if txt_or_csv_path.endswith('.txt'):
+    if txt_or_csv_path is not None and json_pos_path is not None:
+        if txt_or_csv_path.endswith('.csv'):
+            df = pd.read_csv(txt_or_csv_path)
+            pos_dict = pd_to_dict(df)
+        elif txt_or_csv_path.endswith('.txt'):
             pos_dict = text_to_dict(txt_or_csv_path)
-        elif txt_or_csv_path.endswith('.csv'):
-            pos_dict = pd_to_dict(txt_or_csv_path)
         else:
             raise ValueError("The path to the .txt or .csv file is not valid.")
+        assert json_pos_path.endswith('.json'), "The path to the .json file is not valid."
 
         pos_write_path = os.path.join(target_directory, session_name + '.pos')
 
-        write_pos(pos_write_path, pos_dict)
+        write_pos(pos_write_path, json_pos_path, pos_dict)
 
 def get_session_name_from_rhd_file_path(rhd_file_path):
     return os.path.basename(rhd_file_path).split('.')[0]
--- src/tests/test_converter.py
+++ src/tests/test_converter.py
@@ -11,9 +11,12 @@ output_path = PROJECT_PATH + r'\test_outputs'
 
 rhd_path = test_dir + r'\sampledata.rhd'
 
+pos_json_path = test_dir + r'\pos_header.json'
+
 pos_csv_path = test_dir + r'\Book1.csv'
 
 pos_txt_path = None
 
 def test_convert_all_files():
-    convert_all_files(output_path, rhd_path, None)
+    convert_all_files(output_path, rhd_path, None, None)
+    convert_all_files(output_path, rhd_path, pos_csv_path, pos_json_path)
