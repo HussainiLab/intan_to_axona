1adab396c19a260b6587d1a63b223b0b02ab15f5
Oliver Shetler <oliver@olivershetler.com>
2022-09-01 11:54:42 -0400

    Added test_converter module.

---
+++ src/tests/test_converter.py
@@ -0,0 +1,19 @@
+import os
+import sys
+PROJECT_PATH = os.getcwd()
+sys.path.append(PROJECT_PATH)
+
+from src.converter import convert_all_files
+
+test_dir = PROJECT_PATH + r'\src\tests'
+
+output_path = PROJECT_PATH + r'\test_outputs'
+
+rhd_path = test_dir + r'\sampledata.rhd'
+
+pos_csv_path = test_dir + r'\Book1.csv'
+
+pos_txt_path = None
+
+def test_convert_all_files():
+    convert_all_files(output_path, rhd_path, None)
