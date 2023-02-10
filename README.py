5b7585b3c55153c1552a5aa19e9ac3ebf339f0f7
Oliver Shetler <53658943+olivershetler@users.noreply.github.com>
2023-02-10 12:13:28 -0500

    Update README.py

--- README.py
+++ README.py
@@ -6,5 +6,11 @@ This is a simple command line utility for converting rhd files to axona files.
 Clone this repository and go into the directory. Then use the following command format to convert the files.
 
 ```
-python manage.py --output {output directory} --rhd {rhd directory} --csv {csv directory} --json {json directory}
+python manage.py --output {output directory} --rhd {rhd path} --csv {csv path} --json {json path}
 ```
+
+The RHD file should contain the raw electrophysiology data that needs to be down-sampled and converted.
+
+The CSV file may contain concurrent position tracking data.
+
+The JSON file may 
